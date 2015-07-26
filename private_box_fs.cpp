5ac8c25ad69d3d694f8796914496fcfb1931a456
Matthew W Carlis <matthew.carlis@sjsu.edu>
2015-07-26 12:00:50 -0700

    Add to .gitignore, user_input private_box_fs.cpp

--- private_box_fs.cpp
+++ private_box_fs.cpp
@@ -23,7 +23,8 @@ namespace private_box_fs_carlis {
 
 std::string encrypt_string(const char *instr, const char *pass_phrase) {
     std::string out_str;
-    CryptoPP::DefaultEncryptorWithMAC encryptor(pass_phrase, new CryptoPP::HexEncoder(new CryptoPP::StringSink(out_str)));
+    CryptoPP::DefaultEncryptorWithMAC encryptor(pass_phrase,
+                new CryptoPP::HexEncoder(new CryptoPP::StringSink(out_str)));
     encryptor.Put((byte *) instr, strlen(instr));
     encryptor.MessageEnd();
     return out_str;
@@ -31,18 +32,21 @@ std::string encrypt_string(const char *instr, const char *pass_phrase) {
 
 std::string decrypt_string(const char *instr, const char *pass_phrase) {
     std::string out_str;
-    CryptoPP::HexDecoder decryptor(new CryptoPP::DefaultDecryptorWithMAC(pass_phrase, new CryptoPP::StringSink(out_str)));
+    CryptoPP::HexDecoder decryptor(new CryptoPP::DefaultDecryptorWithMAC(pass_phrase,
+                new CryptoPP::StringSink(out_str)));
     decryptor.Put((byte *) instr, strlen(instr));
     decryptor.MessageEnd();
     return out_str;
 }
 
 int main(void) {
-    std::cout << "It's working?" << std::endl;
+    private_box_fs_carlis::private_box_fs my_box;
     std::string cypher_text;
     std::string message;
-    private_box_fs_carlis::private_box_fs my_box;
-    cypher_text = encrypt_string("This is a funny test my friend.", "password123");
+    std::cout << "Enter a string:";
+    std::getline(std::cin, message);
+    std::cout << message << std::endl;
+    cypher_text = encrypt_string(message.c_str(), "password123");
     std::cout << &cypher_text << std::endl;
     message = decrypt_string(cypher_text.c_str(), "password123");
     std::cout << message << std::endl;
@@ -50,3 +54,5 @@ int main(void) {
     my_box.close();
     return 0;
 }
+
+
